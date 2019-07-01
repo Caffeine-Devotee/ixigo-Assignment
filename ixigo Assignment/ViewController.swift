eb1f5fcc75d6b8bd1f63268f78c4c0ced90c0b94
Gaurav Nayak <gauravnayak@iiitdmj.ac.in>
2019-07-01 15:47:40 +0530

    Initial Commit

---
+++ ixigo Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ixigo Assignment
+//
+//  Created by GAURAV NAYAK on 01/07/19.
+//  Copyright © 2019 GAURAV NAYAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
